d1072148af8c225c4e5c7f271fb42c8648d084a1
PaavoNelimarkka <paavo.nelimarkka@gmail.com>
2014-11-19 13:02:01 +0200

    big ass commit

---
+++ js/nav-scroll.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+
+    /* ======= Scrollspy ======= */
+    // $('body').scrollspy({ target: '#nav', offset: 400});
+    
+    /* ======= Fixed nav when scrolled ======= */
+    
+    $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 15) {
+             $('#nav-bg').addClass('nav-bg');
+         }
+         else {
+             $('#nav-bg').removeClass('nav-bg');
+         }
+    });
+   
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+		
+	});
+
+});
